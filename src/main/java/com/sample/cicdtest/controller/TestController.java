b28d8417b7ed0e4ac9f0073554355bd5045792ae
junsoo1003 <royalex@dgu.ac.kr>
2025-01-10 01:19:22 +0900

    test2 추가

--- src/main/java/com/sample/cicdtest/controller/TestController.java
+++ src/main/java/com/sample/cicdtest/controller/TestController.java
@@ -1,18 +1,23 @@
 package com.sample.cicdtest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String hello() {
         return "hello world";
     }
 
-    @RequestMapping("/test1")
+    @GetMapping("/test1")
     public String test1() {
         return "test1";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test2";
     }
 }
